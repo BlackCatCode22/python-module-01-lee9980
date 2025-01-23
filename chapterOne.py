1bac14483d6a4908b4a8031a2bd9b66257468f8f
lee223 <alsilva33330@gmail.com>
2025-01-22 23:08:51 -0800

    Add files via upload

---
+++ chapterOne.py
@@ -0,0 +1,48 @@
+# Anthony Silva
+# CIT 95
+# 1/22/2025
+
+
+# Exercise 1: What is the function of the secondary memory in a computer?
+# answer: c) Store information for the long term, even beyond a power cycle
+
+# Exercise 2: What is a program?
+# answer: programs are a set of instructions a computer follows to preform various tasks
+
+# Exercise 3: What is the difference between a compiler and an interpreter?
+# answer: a compiler translates a piece of code all at once while a interperter does it
+# line by line.
+
+# Exercise 4: Which of the following contains “machine code”?
+# a) The Python interpreter
+
+# Exercise 5: What is wrong with the following code:
+# >>> primt 'Hello world!'
+# File "<stdin>", line 1
+#  primt 'Hello world!'
+#                    ^
+# SyntaxError: invalid syntax
+# >>>
+#  answer: it is a syntax error, a misspelling of the keyword "print"
+
+# Exercise 6: Where in the computer is a variable such as “x” stored after the following Python line finishes?
+# x = 123
+# answer: b) Main Memory
+
+# Exercise 7: What will the following program print out:
+# x = 43
+# x = x - 1
+# print(x)
+# answer: 42
+
+# Exercise 8: Explain each of the following using an example of a human capability: (1) Central processing unit, (2) Main Memory, (3) Secondary Memory, (4) Input Device, and (5) Output Device. For example,
+# “What is the human equivalent to a Central Processing Unit”?
+# answer: to answer this ill go one by one.
+# central processing unit will of course be the brain, processing information, and instructions for example.
+# the main memory would specifically be the prefrontal lobe, as it stores all our short term memories.
+# the secondary memory would be our hippocampus (part of the brain) as it stores our long term memories.
+# the input device could be our 5 senses since we are basically gaining input from everything we see,
+# taste, touch, smell, and hear.
+# And lastly our output device could be our reactions? how we react to what we "input". For example, responding
+# positively or negatively to some ones comment about a shirt we're wearing.
+
